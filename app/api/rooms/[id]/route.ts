ba110f579b2cb2319ea11b477630922d5651abd5
Apurbo810 <114826531+Apurbo810@users.noreply.github.com>
2026-03-14 00:13:02 +0600

    fix route

--- app/api/rooms/[id]/route.ts
+++ app/api/rooms/[id]/route.ts
@@ -1,19 +1,22 @@
 import { connectDB } from "@/lib/mongodb";
 import Room from "@/models/Room";
 import mongoose from "mongoose";
+import { NextRequest } from "next/server";
 
 export async function GET(
-  req: Request,
-  { params }: { params: { id: string } }
+  req: NextRequest,
+  context: { params: Promise<{ id: string }> }
 ) {
   await connectDB();
 
+  const { id } = await context.params;
+
   // validate id
-  if (!mongoose.Types.ObjectId.isValid(params.id)) {
+  if (!mongoose.Types.ObjectId.isValid(id)) {
     return Response.json({ message: "Invalid room id" }, { status: 400 });
   }
 
-  const room = await Room.findById(params.id);
+  const room = await Room.findById(id);
 
   if (!room) {
     return Response.json({ message: "Room not found" }, { status: 404 });
